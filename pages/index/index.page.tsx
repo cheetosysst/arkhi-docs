0dcb462de91a2c059452ce93cb8743523f5149f0
cheetosysst <thect@protonmail.com>
2023-10-25 17:40:38 +0800

    feat: refactor and update index page Refactor hero to an seperate  component New features section resize some layout

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import { Island } from "arkhi/client";
+import React, { type ReactElement } from "react";
 import { Head } from "arkhi/client";
 
 import "./index.css";
-import { Github } from "lucide-react";
+import { Github, Palmtree, type LucideIcon } from "lucide-react";
 
 export { Page };
 export const PrefetchSetting = { mode: "hover" };
@@ -15,58 +14,132 @@ function Page() {
 				<title>Index Page - Arkhi</title>
 				<link rel="icon" type="image/x-icon" href="favicon.ico" />
 			</Head>
-			<div className="flex flex-col-reverse lg:flex-row py-20 justify-around">
-				<div className="text-center lg:text-left">
-					<h1 className="lg:text-5xl pt-10 text-2xl font-bold bg-gradient-to-r leading-10 lg:leading-[4rem] from-pink-500 via-purple-700 to-purple-500 bg-clip-text">
-						<span className="text-transparent text-6xl">
-							Arkhi.js
-							<br />
-						</span>
-						Islands Architecture
+			<Hero />
+			<Features />
+		</>
+	);
+}
+
+function Hero() {
+	return (
+		<div className="flex flex-col-reverse lg:flex-row pt-5 pb-14 justify-around">
+			<div className="text-center lg:text-left">
+				<h1 className="lg:text-5xl pt-10 text-2xl font-bold bg-gradient-to-r leading-10 lg:leading-[4rem] from-pink-500 via-purple-700 to-purple-500 bg-clip-text">
+					<span className="text-transparent text-6xl">
+						Arkhi.js
 						<br />
-						Frontend Framework
-					</h1>
-					<span className="lg:text-2xl capitalize text-xl font-medium text-white/50">
-						Blazingly fast hydration
 					</span>
-					<div className="flex gap-2 justify-center lg:justify-start mt-10 flex-wrap">
-						<a
-							href="/"
-							className="py-2 px-4 font-semibold bg-gradient-to-r from-purple-700 to-purple-500 hover:from-red-500 hover:to-violet-500 transition-all duration-500 drop-shadow-md rounded-3xl capitalize"
-						>
-							getting started
-						</a>
-						<a
-							href="/"
-							className="py-2 px-4 font-medium drop-shadow-md bg-white/20 hover:bg-white/30 rounded-3xl capitalize"
-						>
-							Documentation
-						</a>
-						<a
-							href="/"
-							className="flex gap-2 items-center justify-center py-2 px-4 font-medium drop-shadow-md bg-white/20 hover:bg-white/30 rounded-3xl capitalize"
-						>
-							<Github />
-							Github
-						</a>
-						<a
-							href="/"
-							className="py-2 px-4 font-medium drop-shadow-md bg-white/20 hover:bg-white/30 rounded-3xl capitalize"
-						>
-							About
-						</a>
-					</div>
-				</div>
-				<div className="flex justify-center relative items-center">
-					<div className="logo-glow" />
-					<img
-						src="arkhi.svg"
-						className=" z-10"
-						alt="logo of arkhi framework"
-						width={350}
-					/>
+					Islands Architecture
+					<br />
+					Frontend Framework
+				</h1>
+				<span className="lg:text-2xl capitalize text-xl font-medium text-white/50">
+					Blazingly fast hydration
+				</span>
+				<div className="flex gap-2 justify-center lg:justify-start mt-10 flex-wrap">
+					<a
+						href="/"
+						className="py-2 px-4 font-semibold bg-gradient-to-r from-purple-700 to-purple-500 hover:from-red-500 hover:to-violet-500 transition-all duration-500 drop-shadow-md rounded-3xl capitalize"
+					>
+						getting started
+					</a>
+					<a
+						href="/"
+						className="py-2 px-4 font-medium drop-shadow-md bg-white/20 hover:bg-white/30 rounded-3xl capitalize"
+					>
+						Documentation
+					</a>
+					<a
+						href="/"
+						className="flex gap-2 items-center justify-center py-2 px-4 font-medium drop-shadow-md bg-white/20 hover:bg-white/30 rounded-3xl capitalize"
+					>
+						<Github />
+						Github
+					</a>
+					<a
+						href="/"
+						className="py-2 px-4 font-medium drop-shadow-md bg-white/20 hover:bg-white/30 rounded-3xl capitalize"
+					>
+						About
+					</a>
 				</div>
 			</div>
-		</>
+			<div className="flex justify-center relative items-center">
+				<div className="logo-glow" />
+				<img
+					src="arkhi.svg"
+					className=" z-10"
+					alt="logo of arkhi framework"
+					width={350}
+				/>
+			</div>
+		</div>
+	);
+}
+
+type Feature = {
+	name: string;
+	icon: ReactElement;
+	description: string;
+};
+
+const features: Array<Feature> = [
+	{
+		name: "Island Architecture",
+		icon: <Palmtree className="drop-shadow-sm" />,
+		description: "lorem ipsum",
+	},
+	{
+		name: "Island Architecture",
+		icon: <Palmtree className="drop-shadow-sm" />,
+		description: "lorem ipsum",
+	},
+	{
+		name: "Island Architecture",
+		icon: <Palmtree className="drop-shadow-sm" />,
+		description: "lorem ipsum",
+	},
+	{
+		name: "Island Architecture",
+		icon: <Palmtree className="drop-shadow-sm" />,
+		description: "lorem ipsum",
+	},
+	{
+		name: "Island Architecture",
+		icon: <Palmtree className="drop-shadow-sm" />,
+		description: "lorem ipsum",
+	},
+	{
+		name: "Island Architecture",
+		icon: <Palmtree className="drop-shadow-sm" />,
+		description: "lorem ipsum",
+	},
+];
+
+function Features() {
+	return (
+		<div className="flex flex-wrap justify-center flex-row gap-4">
+			{features.map((item) => (
+				<FeatureCard className="w-80" {...item} />
+			))}
+		</div>
+	);
+}
+function FeatureCard({
+	className = "",
+	name,
+	icon,
+	description,
+}: { className?: string } & Feature) {
+	return (
+		<div
+			className={`flex flex-col bg-neutral-800 hover:bg-neutral-600 transition-colors p-4 rounded-xl drop-shadow-md ${className}`}
+		>
+			<span className="bg-white/10 aspect-square w-fit p-2 m-2 rounded-md">
+				{icon}
+			</span>
+			<h2 className="text-lg font-medium pl-2">{name}</h2>
+			<p className="pl-2 font-medium text-white/80">{description}</p>
+		</div>
 	);
 }
