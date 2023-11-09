d3b15528487a09007fa2ed76a8b938a8f53b9594
cheetosysst <thect@protonmail.com>
2023-11-09 20:40:48 +0800

    feat: some rwd adjustment

--- renderer/PageShell.tsx
+++ renderer/PageShell.tsx
@@ -28,8 +28,8 @@ function PageShell({
 function Layout({ children }: PropsWithChildren) {
 	return (
 		<>
+			<Navbar />
 			<div className="max-w-screen-xl mx-auto min-h-[100dvh] flex-col flex gap-4 py-8 px-4 sm:px-6 md:px-8">
-				<Navbar />
 				{children}
 			</div>
 		</>
@@ -38,105 +38,107 @@ function Layout({ children }: PropsWithChildren) {
 
 function Navbar({ children }: PropsWithChildren) {
 	return (
-		<div className="mx-auto bg-nav w-full sticky flex justify-between items-center z-20 top-0 bg-transparent">
-			<div>
-				<Link
-					href="/"
-					className="flex transition-colors cursor-pointer p-2 rounded-md hover:bg-white/30 items-center text-4xl font-bold gap-2"
-				>
-					<img src="/arkhi.svg" width={32} height={32} />
-					<h1 className="">ARKHI</h1>
-				</Link>
-			</div>
-			{/* normal nav links */}
-			<ul className="lg:flex hidden gap-2 text-lg">
-				<li>
+		<div className="max-w-screen-xl mx-auto sticky top-0 bg-gradient-to-b from-[#212121] lg:px-20 p-4 to-[#21212100]">
+			<div className="bg-nav w-full flex justify-between items-center z-20">
+				<div>
 					<Link
-						className="hover:bg-white/50 rounded-md transition-colors p-2"
-						href="/docs"
+						href="/"
+						className="flex transition-colors cursor-pointer p-2 rounded-md hover:bg-white/30 items-center text-4xl font-bold gap-2"
 					>
-						Docs
+						<img src="/arkhi.svg" width={32} height={32} />
+						<h1 className="">ARKHI</h1>
 					</Link>
-				</li>
-				<li>
-					<Link
-						className="hover:bg-white/50 rounded-md transition-colors p-2"
-						href="/guides"
-					>
-						Guides
-					</Link>
-				</li>
-				<li>
-					<Link
-						className="hover:bg-white/50 rounded-md transition-colors p-2"
-						href="https://github.com/cheetosysst/arkhi"
-					>
-						Github
-					</Link>
-				</li>
-				<li>
-					<Link
-						className="hover:bg-white/50 rounded-md transition-colors p-2"
-						href="/about"
-					>
-						About
-					</Link>
-				</li>
-			</ul>
-
-			<div className="relative flex lg:hidden">
-				<input
-					type="checkbox"
-					className="hidden peer/toggle"
-					id="toggle"
-				/>
-				<label
-					htmlFor="toggle"
-					className="text-white hover:bg-white/20 transition-colors py-2 px-4 rounded cursor-pointer"
-				>
-					<Menu />
-				</label>
-				<ul
-					className="hidden absolute flex-wrap gap-1 w-fit top-5 -translate-x-1/2 mt-4 peer-checked/toggle:flex p-4 bg-neutral-900/90 rounded-xl"
-					id="content"
-				>
-					<li className="w-full">
+				</div>
+				{/* normal nav links */}
+				<ul className="lg:flex hidden gap-2 text-lg">
+					<li>
 						<Link
-							className="hover:bg-white/50 flex items-center gap-2 rounded-md transition-colors p-2"
+							className="hover:bg-white/50 rounded-md transition-colors p-2"
 							href="/docs"
 						>
-							<FileText size={20} />
 							Docs
 						</Link>
 					</li>
-					<li className="w-full">
+					<li>
 						<Link
-							className="hover:bg-white/50 flex items-center gap-2 rounded-md transition-colors p-2"
+							className="hover:bg-white/50 rounded-md transition-colors p-2"
 							href="/guides"
 						>
-							<GraduationCap size={20} />
 							Guides
 						</Link>
 					</li>
-					<li className="w-full">
+					<li>
 						<Link
-							className="hover:bg-white/50 flex items-center gap-2 rounded-md transition-colors p-2"
+							className="hover:bg-white/50 rounded-md transition-colors p-2"
 							href="https://github.com/cheetosysst/arkhi"
 						>
-							<Github size={20} />
 							Github
 						</Link>
 					</li>
-					<li className="w-full">
+					<li>
 						<Link
-							className="hover:bg-white/50 flex items-center gap-2 rounded-md transition-colors p-2"
+							className="hover:bg-white/50 rounded-md transition-colors p-2"
 							href="/about"
 						>
-							<Info size={20} />
 							About
 						</Link>
 					</li>
 				</ul>
+
+				<div className="relative flex lg:hidden">
+					<input
+						type="checkbox"
+						className="hidden peer/toggle"
+						id="toggle"
+					/>
+					<label
+						htmlFor="toggle"
+						className="text-white hover:bg-white/20 transition-colors py-2 px-4 rounded cursor-pointer"
+					>
+						<Menu />
+					</label>
+					<ul
+						className="hidden absolute flex-wrap gap-1 w-fit top-5 -translate-x-1/2 mt-4 peer-checked/toggle:flex p-4 bg-neutral-900/90 rounded-xl"
+						id="content"
+					>
+						<li className="w-full">
+							<Link
+								className="hover:bg-white/50 flex items-center gap-2 rounded-md transition-colors p-2"
+								href="/docs"
+							>
+								<FileText size={20} />
+								Docs
+							</Link>
+						</li>
+						<li className="w-full">
+							<Link
+								className="hover:bg-white/50 flex items-center gap-2 rounded-md transition-colors p-2"
+								href="/guides"
+							>
+								<GraduationCap size={20} />
+								Guides
+							</Link>
+						</li>
+						<li className="w-full">
+							<Link
+								className="hover:bg-white/50 flex items-center gap-2 rounded-md transition-colors p-2"
+								href="https://github.com/cheetosysst/arkhi"
+							>
+								<Github size={20} />
+								Github
+							</Link>
+						</li>
+						<li className="w-full">
+							<Link
+								className="hover:bg-white/50 flex items-center gap-2 rounded-md transition-colors p-2"
+								href="/about"
+							>
+								<Info size={20} />
+								About
+							</Link>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
