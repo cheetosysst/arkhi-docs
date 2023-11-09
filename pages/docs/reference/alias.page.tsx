3066a55a39221deb4166a0a5f16319e9d3da606d
cheetosysst <thect@protonmail.com>
2023-11-09 21:26:12 +0800

    feat: reference docs

---
+++ pages/docs/reference/alias.page.tsx
@@ -0,0 +1,18 @@
+import { Head } from "arkhi/client";
+import Layout from "../../layout";
+import Article from "#/content/docs/reference/alisas.md";
+
+export { Page };
+export const PrefetchSetting = { mode: "hover" };
+
+function Page() {
+	return (
+		<Layout>
+			<Head>
+				<title>Index Page - Arkhi</title>
+				<link rel="icon" type="image/x-icon" href="favicon.ico" />
+			</Head>
+			<Article />
+		</Layout>
+	);
+}
