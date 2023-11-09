e9a489449f9cca516f4ddb8249e449f6b54d73d5
cheetosysst <thect@protonmail.com>
2023-11-09 21:49:22 +0800

    feat: docs index page

--- pages/docs/index.page.tsx
+++ pages/docs/index.page.tsx
@@ -1,19 +1,36 @@
-import { Head } from "arkhi/client";
-import Layout from "../layout";
-import Article from "#/content/index.md";
-import type { Docs } from "#/libs/files";
+import { Head, Link } from "arkhi/client";
+import Layout, { LinksForIndex } from "../layout";
+import { getDocs, getSectionArticle } from "#/libs/files";
 
 export { Page };
 export const PrefetchSetting = { mode: "hover" };
 
 function Page() {
+	// const articles = getSectionArticle("start");
+	const docs = getDocs();
+
 	return (
 		<Layout>
 			<Head>
-				<title>Index Page - Arkhi</title>
+				<title>Start - Arkhi</title>
 				<link rel="icon" type="image/x-icon" href="favicon.ico" />
 			</Head>
-			<Article />
+			<h1 className="text-4xl pl-2 mb-5">Start</h1>
+
+			<div className="flex flex-col gap-4">
+				{Object.keys(docs)
+					.map((key) => docs[key])
+					.sort((a, b) => a.weight - b.weight)
+					.map((chapter) => (
+						<Link
+							className="p-6 rounded-xl drop-shadow-lg bg-neutral-800 hover:bg-neutral-700 capitalize font-medium text-xl"
+							href={`/docs/${chapter.name}`}
+							key={chapter.name}
+						>
+							{chapter.name}
+						</Link>
+					))}
+			</div>
 		</Layout>
 	);
 }
